0e2cecce644336af9405571cb7d6f45803c02883
hirossan <haruto405329@gmail.com>
2019-12-18 07:03:29 +0900

    f commit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import rumps
 import datetime
-import subprocess
 import osascript
 
 TIMER_MODE = 'timer'
-go_home_time = '18:00:00'
 track_osa = 'tell application "Spotify" to name of current track as string'
 artist_osa = 'tell application "Spotify" to artist of current track as string'
 play_stop_osa = 'tell application "Spotify" to playpause'
 next_osa = 'tell application "Spotify" to next track'
 back_osa = 'tell application "Spotify" to back track'
-ICON_NEUTRAL = 'clear.png'
+
 
 @rumps.clicked(u'一時停止')
 def switchTimer(sender):
@@ -23,7 +21,6 @@ def switchTimer(sender):
 
 @rumps.timer(5)
 def dispTimer(sender):
-    #app.icon = ICON_NEUTRAL
     track = osascript.run(track_osa)[1]
     artist = osascript.run(artist_osa)[1]
     app.title = artist + " - " + track
@@ -35,9 +32,3 @@ if __name__ == "__main__":
     app.run()
 
 
-#get = subprocess.Popen(track_cmd.split(), stdout=subprocess.PIPE)
-#print(get.communicate())
-#code,out,err = osascript.run(track_osa)
-#print(out)
-#code,out,err = osascript.run(artist_osa)
-#print(out)
